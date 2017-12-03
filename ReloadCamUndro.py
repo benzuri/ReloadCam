7cb760088d9526c5bcdf009b43e74a4e6503fb76
jograben <jograben@gmail.com>
2017-12-03 15:18:43 +0100

    Update ReloadCamUndro.py

--- ReloadCamUndro.py
+++ ReloadCamUndro.py
@@ -12,22 +12,20 @@ def ReloadCamUndro():
    with open(ReloadCam.cccamPath, 'r') as file :
       
       # Replace the target string 
-      lines = [( '' + line.replace('C: ', 'C:').replace('|1|0', '').replace('DEFAULT:1', '').replace('DEFAULT:0', '').replace(' ', '|').rstrip('\n') + '|1|0' + "\n" ) for line in open(ReloadCam.cccamPath) ]
+      lines = [( '' + line.replace('C: ', 'C:').replace('|1|0', '').replace('DEFAULT:1', '#').replace('DEFAULT:0', '#').replace(' ', '|').rstrip('\n') + '|1|0' + "\n" ) for line in open(ReloadCam.cccamPath) ]
 
    # Write the file out again
    with open(ReloadCam.cccamPath, 'w') as file: 
    
       #IKS
       if lines == 0:
-         file.write('DEFAULT:0\n')
+         file.write('DEFAULT:1\n')
       #CCCAM
       file.writelines(lines)
 
    import time
    # Reinicio de Spring.apk (al salir de qpython cargará de nuevo listas, canales, ...)
    #os.system("adb shell am force-stop com.dvb.spring.home")
-   # Se copia el archivo al sistema
-   #os.system("adb push " + ReloadCam.cccamPath + " /data/data/com.dvb.spring.home/app_tmp/card_server.cfg")
    import shutil
    shutil.copy2(ReloadCam.cccamPath, ReloadCam.cccamBin)
    
