6b5116d76c42565cd0b71d0d8a6c5230ed05c38c
jograben <jograben@gmail.com>
2017-12-03 22:27:12 +0100

    Update ReloadCam_Versions.py

--- ReloadCam_Versions.py
+++ ReloadCam_Versions.py
@@ -6,6 +6,8 @@
 
 Versions = {
     "ReloadCamUndro" : 1,
+    "ReloadCam_CCcamTester" : 1,
+    "ReloadCam_CriptoBlock" : 1,
     "ReloadCam_Arguments" : 2,
     "ReloadCam_Helper" : 3,
     "ReloadCam_Main" : 4,
