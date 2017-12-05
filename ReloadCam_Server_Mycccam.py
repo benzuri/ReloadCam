cf50ffb5f1aab79405d5b266024b6bba5379d609
jograben <jograben@gmail.com>
2017-12-05 09:48:55 +0100

    Update ReloadCam_Server_Mycccam.py

--- ReloadCam_Server_Mycccam.py
+++ ReloadCam_Server_Mycccam.py
@@ -7,7 +7,7 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 1
+    return 2
 
 #Filename must start with Server, classname and argument must be the same!
 class Mycccam(ReloadCam_Main.Server):
