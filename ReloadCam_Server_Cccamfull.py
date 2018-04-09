a87dbe792acc23dda6b5bea5a1737dd48f6fe65c
benzuri <jograben@gmail.com>
2018-04-09 10:14:45 +0200

    Create ReloadCam_Server_Cccamfull.py

---
+++ ReloadCam_Server_Cccamfull.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+import ReloadCam_Main, ReloadCam_Helper
+
+def GetVersion():
+    return 1
+
+#Filename must start with Server, classname and argument must be the same!
+class Cccamfull(ReloadCam_Main.Server):
+
+    def GetUrl(self):
+        #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
+        realUrl = ReloadCam_Helper.Decrypt("maanpLZ7fKHIz9LC0V6YqKCvb7Dh0pvV08mWlZaXpK574s3c")
+        return realUrl
+
+    def GetClines(self):
+        print "Now getting Cccamfull clines!"
+        cccamfullClines = []
+        cccamfullClines.append(self.__GetCccamfullCline())
+        cccamfullClines = filter(None, cccamfullClines)
+        if len(cccamfullClines) == 0: print "No Cccamfull lines retrieved"
+        return cccamfullClines
+
+    def __GetCccamfullCline(self):
+        htmlCode = ReloadCam_Helper.GetHtmlCode(None, self.GetUrl())
+        cline = ReloadCam_Helper.FindStandardClineInText(htmlCode)
+        if cline != None and ReloadCam_Helper.TestCline(cline):
+            return cline
+return None
