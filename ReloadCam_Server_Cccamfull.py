f16c0646125bf4e9ef54279109f16b7754262b4d
benzuri <jograben@gmail.com>
2018-04-10 22:22:19 +0200

    Update ReloadCam_Server_Cccamfull.py

--- ReloadCam_Server_Cccamfull.py
+++ ReloadCam_Server_Cccamfull.py
@@ -30,4 +30,4 @@ class Cccamfull(ReloadCam_Main.Server):
         cline = ReloadCam_Helper.FindStandardClineInText(htmlCode)
         if cline != None and ReloadCam_Helper.TestCline(cline):
             return cline
-return None
+        return None
