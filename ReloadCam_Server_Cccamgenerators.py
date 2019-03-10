7d3dbd5f5f6ce45152fc629a4653b0fa8d37bdc7
benzuri <jograben@gmail.com>
2019-03-10 19:10:48 +0100

    Update ReloadCam_Server_Cccamgenerators.py

--- ReloadCam_Server_Cccamgenerators.py
+++ ReloadCam_Server_Cccamgenerators.py
@@ -29,4 +29,4 @@ class Cccamgenerators(ReloadCam_Main.Server):
         cline = ReloadCam_Helper.FindStandardClineInText(htmlCode)
         if cline != None and ReloadCam_Helper.TestCline(cline):
             return cline
-return None
+        return None
