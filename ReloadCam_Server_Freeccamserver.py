867b17c262b549a3a78ab700bbe0668c063d96bf
benzuri <jograben@gmail.com>
2019-03-10 14:55:10 +0100

    Create ReloadCam_Server_Freeccamserver.py

---
+++ ReloadCam_Server_Freeccamserver.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+
+import ReloadCam_Main, ReloadCam_Helper
+
+def GetVersion():
+    return 1
+
+#Filename must start with Server, classname and argument must be the same!
+class Freeccamserver(ReloadCam_Main.Server):
+
+    def GetUrl(self):
+        #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
+        realUrl = ReloadCam_Helper.Decrypt('maanpH1wfNjX0dTEx5Kfppm1t7Lkk8_ezpOXpJiZcqiy5pea38nU')
+        return realUrl
+
+    def GetClines(self):
+        print "Now getting Freeccamserver clines!"
+        freeccamserverClines = []
+        freeccamserverClines.append(self.__GetFreeccamserverClinesCline())
+        freeccamserverClines = filter(None, freeccamserverClines)
+        if len(freeccamserverClines) == 0: print "No Freeccamserver lines retrieved"
+        return freeccamserverClines
+
+    def __GetFreeccamserverClinesCline(self):
+        htmlCode = ReloadCam_Helper.GetHtmlCode(None, self.GetUrl())
+        cline = ReloadCam_Helper.FindStandardClineInText(htmlCode)
+        if cline != None and ReloadCam_Helper.TestCline(cline):
+            return cline
+return None
